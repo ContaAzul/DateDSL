d7c52bd02d0371798230d88757504f5f5deb3fc6
Daniel Passos <danpassos@gmail.com>
2011-11-28 12:40:45 -0200

    Documentation (JavaDoc) of TimeUnit

--- src/main/java/com/unimondes/dsl/datedsl/TimeUnit.java
+++ src/main/java/com/unimondes/dsl/datedsl/TimeUnit.java
@@ -6,25 +6,61 @@ public class TimeUnit {
 	private final int size;
 	private boolean workingDay = false;
 
+    /**
+     *
+     * Create timeunit
+     *
+     * @param type int (Calendar.FIELD_NAME)
+     * @param size size of unit
+     *
+     */
 	public TimeUnit(int type, int size) {
 		this.type = type;
 		this.size = size;
 	}
 
+    /**
+     *
+     * Create timeunit
+     *
+     * @param type int (Calendar.FIELD_NAME)
+     * @param size size of unit
+     * @param workingDay workingday
+     *
+     */
 	public TimeUnit(int type, int size, boolean workingDay) {
 		this.type = type;
 		this.size = size;
 		this.workingDay = true;
 	}
 
+    /**
+     *
+     * Get type
+     *
+     * @return int (Calendar.FIELD_NAME)
+     *
+     */
 	public int getType() {
 		return type;
 	}
 
+    /**
+     *
+     * Get size
+     *
+     * @return int
+     *
+     */
 	public int getSize() {
 		return size;
 	}
 
+    /**
+     *
+     * Unit is workday?
+     *
+     */
 	public boolean isWorkingDay() {
 		return workingDay;
 	}
