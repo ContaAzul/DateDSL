2fe1d0a4d262bb557cb76b84c06b59df653332a0
Daniel Passos <danpassos@gmail.com>
2011-11-28 11:26:21 -0200

    Documentation (JavaDoc) of DateDsl

--- src/main/java/com/unimondes/dsl/datedsl/DateDsl.java
+++ src/main/java/com/unimondes/dsl/datedsl/DateDsl.java
@@ -12,35 +12,45 @@ public final class DateDsl {
 	}
 	
 	/**
-	 * Creation du builder a l'aide d'une date précise.
-	 * @param date Date 
+	 * Creation builder with a specific date.
+     *
+	 * @param date Date
+     *
 	 * @return DateBuilder
+     *
 	 */
 	public static DateBuilder date(Date date) {
 		return new DateBuilder(date);
 	}
 
 	/**
-	 * Creation du builder avec une date précise représentée par un 
-	 * Calendar.
-	 * @param calendar Calendar
+     * Creation builder with a specific calendar.
+     *
+     * @param calendar Calendar
+     *
 	 * @return DateBuilder
+     *
 	 */
 	public static DateBuilder date(Calendar calendar) {
 		return new DateBuilder(calendar);
 	}
 
 	/**
-	 * Création du builder initialisé a la date de création.
+	 *
+     * Create builder has initialized the creation date.
+     *
 	 * @return DateBuilder
+     *
 	 */
 	public static DateBuilder now() {
 		return new DateBuilder();
 	}
 
 	/**
-	 * Création du builder initialisé a la date du lendemain (hh/mm/ss = celle de creation).
+	 * Create the builder initialized to the date of the next day
+     *
 	 * @return DateBuilder
+     *
 	 */
 	public static DateBuilder tomorrow() {
 		DateBuilder date = new DateBuilder(new Date());
@@ -48,50 +58,132 @@ public final class DateDsl {
 	}
 
 	/**
-	 * Création du builder initialisé a la date de la veille (hh/mm/ss = celle de creation).
+	 *
+     * Create the builder has initialized the day before the date of
+     *
 	 * @return DateBuilder
+     *
 	 */
 	public static DateBuilder yesterday() {
 		DateBuilder date = new DateBuilder(new Date());
 		return date.subtract(days(1));
 	}
 
+    /**
+     *
+     * Create the builder of empty date
+     *
+     * @return DateBuilder
+     *
+     */
 	public static DateBuilder emptyDate() {
 		Calendar cal = Calendar.getInstance();
 		cal.clear();
 		return new DateBuilder(cal.getTime());
 	}
-	
-	public static TimeUnit months(int n) {
-		return new TimeUnit(Calendar.MONTH, n);
-	}
 
-	public static TimeUnit years(int n) {
-		return new TimeUnit(Calendar.YEAR, n);
-	}
-
+    /**
+     *
+     * Create unit of days
+     *
+     * @param n number of days
+     *
+     * @return TimeUnit
+     *
+     */
 	public static TimeUnit days(int n) {
 		return new TimeUnit(Calendar.DAY_OF_MONTH, n);
 	}
 
-	public static TimeUnit workingDays(int n) {
-		return new TimeUnit(Calendar.DAY_OF_MONTH, n, true);
+    /**
+     *
+     * Create unit of months
+     *
+     * @param n number of months
+     *
+     * @return TimeUnit
+     *
+     */
+	public static TimeUnit months(int n) {
+		return new TimeUnit(Calendar.MONTH, n);
 	}
 
+    /**
+     *
+     * Create unit of years
+     *
+     * @param n number of years
+     *
+     * @return TimeUnit
+     *
+     */
+	public static TimeUnit years(int n) {
+		return new TimeUnit(Calendar.YEAR, n);
+	}
+
+    /**
+     *
+     * Create unit of hours
+     *
+     * @param n number of hours
+     *
+     * @return TimeUnit
+     *
+     */
 	public static TimeUnit hours(int n) {
 		return new TimeUnit(Calendar.HOUR, n);
 	}
 
+    /**
+     *
+     * Create unit of minutes
+     *
+     * @param n number of minutes
+     *
+     * @return TimeUnit
+     *
+     */
 	public static TimeUnit minutes(int n) {
 		return new TimeUnit(Calendar.MINUTE, n);
 	}
 
-	public static TimeUnit secondes(int n) {
+    /**
+     *
+     * Create unit of seconds
+     *
+     * @param n number of seconds
+     *
+     * @return TimeUnit
+     *
+     */
+	public static TimeUnit seconds(int n) {
 		return new TimeUnit(Calendar.SECOND, n);
 	}
 
+    /**
+     *
+     * Create unit of milliseconds
+     *
+     * @param n number of seconds
+     *
+     * @return TimeUnit
+     *
+     */
 	public static TimeUnit milliSecondes(int n) {
 		return new TimeUnit(Calendar.MILLISECOND, n);
 	}
+
+    /**
+     *
+     * Create unit of days ( only workind days )
+     *
+     * @param n number of days
+     *
+     * @return TimeUnit
+     *
+     */
+    public static TimeUnit workingDays(int n) {
+   		return new TimeUnit(Calendar.DAY_OF_MONTH, n, true);
+   	}
 
 }
