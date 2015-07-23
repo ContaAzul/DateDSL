6ac6aab29641588dbc2ec366c0d879c7cc25e5fa
tadeubonatti <tadeu@contaazul.com>
2015-07-23 17:16:49 -0300

    Formatacao enum WeekDay

--- src/main/java/com/contaazul/dsl/WeekDays.java
+++ src/main/java/com/contaazul/dsl/WeekDays.java
@@ -3,8 +3,13 @@ package com.contaazul.dsl;
 import java.util.Calendar;
 
 public enum WeekDays {
-	SUNDAY(Calendar.SUNDAY), MONDAY(Calendar.MONDAY), TUESDAY(Calendar.TUESDAY), WEDNESDAY(Calendar.WEDNESDAY), THURSDAY(
-			Calendar.THURSDAY), FRIDAY(Calendar.FRIDAY), SATURDAY(Calendar.SATURDAY);
+	SUNDAY(Calendar.SUNDAY),
+	MONDAY(Calendar.MONDAY),
+	TUESDAY(Calendar.TUESDAY),
+	WEDNESDAY(Calendar.WEDNESDAY),
+	THURSDAY(Calendar.THURSDAY),
+	FRIDAY(Calendar.FRIDAY),
+	SATURDAY(Calendar.SATURDAY);
 
 	final int calendaWeekday;
 
